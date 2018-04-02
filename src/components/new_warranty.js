baa62c4345c41cd498e4e877a9fd8a5c1073be91
Pasi Kovanen <pasi.kovanen@vincit.fi>
2018-04-02 22:31:39 +0300

    redux yak shaving

---
+++ src/components/new_warranty.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewWarranty extends Component {
+    render() {
+        return (
+            <div>
+                moikka
+            </div>
+        );
+    }
+}
+
+export default NewWarranty;
