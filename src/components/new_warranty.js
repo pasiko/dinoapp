ca6d8bb6eedc2efba52c20226fcbce8a2b4c54d2
Pasi Kovanen <pasi.kovanen@vincit.fi>
2018-04-03 23:27:22 +0300

    form submit log to console

--- src/components/new_warranty.js
+++ src/components/new_warranty.js
@@ -1,58 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
-const NewWarranty = props => {
-    const { pristine, submitting } = props;
-
-    function handleSubmit(values) {
-        // todo...
+class NewWarranty extends Component {
+    onSubmit(values) {
+        console.log(values);
+    }    
+    
+    render() {
+        const { handleSubmit } = this.props;
+    
+        return (
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <div>
+                    Add new warranty
+                </div>
+                <div>
+                    <label>Name of the thing</label>
+                    <div>
+                        <Field
+                            name="name"
+                            component="input"
+                            type="text"
+                            placeholder="Name of your purchase"
+                        />
+                    </div>
+                </div>
+                <div>
+                    <label>Date of purchase</label>
+                    <div>
+                        <Field
+                            name="date"
+                            component="input"
+                            type="date"
+                            placeholder="When did you buy it"
+                        />
+                    </div>
+                </div>
+                <div>
+                    <label>Warranty ends</label>
+                    <div>
+                        <Field
+                            name="end_of_warranty"
+                            component="input"
+                            type="date"
+                            placeholder="When does the warranty end"
+                        />
+                    </div>
+                </div>
+                <div>
+                    <button type="submit">
+                    Submit
+                    </button>
+                </div>
+            </form>
+        );
     }
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                Add new warranty
-            </div>
-            <div>
-                <label>Name of the thing</label>
-                <div>
-                    <Field
-                        name="name"
-                        component="input"
-                        type="text"
-                        placeholder="Name of your purchase"
-                    />
-                </div>
-            </div>
-            <div>
-                <label>Date of purchase</label>
-                <div>
-                    <Field
-                        name="date"
-                        component="input"
-                        type="date"
-                        placeholder="When did you buy it"
-                    />
-                </div>
-            </div>
-            <div>
-                <label>Warranty ends</label>
-                <div>
-                    <Field
-                        name="end_of_warranty"
-                        component="input"
-                        type="date"
-                        placeholder="When does the warranty end"
-                    />
-                </div>
-            </div>
-            <div>
-                <button type="submit" disabled={pristine || submitting}>
-                  Submit
-                </button>
-            </div>
-        </form>
-    );
 }
 
 export default reduxForm( {
