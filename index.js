e8f2c98aaf031a02e44066c6a550de17d560b55c
Kari Uusitalo <kuusitalo@gmail.com>
2018-04-04 21:44:46 +0300

    Fixed environment variable PORT -handling in BE.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.post('/warranties', (req, res) => {
   res.json()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
