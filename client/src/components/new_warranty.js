490951aba909030700439fb483b22569bd00a7a2
Pasi Kovanen <pasi.kovanen@vincit.fi>
2018-04-14 22:32:14 +0300

    Add back button

--- client/src/components/new_warranty.js
+++ client/src/components/new_warranty.js
@@ -3,6 +3,7 @@ import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { createWarranty } from '../actions';
 import { withRouter } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 // Fix to get file input working with latest React
 // https://github.com/erikras/redux-form/issues/3686
@@ -33,62 +34,66 @@ class NewWarranty extends Component {
     
     render() {
         const { handleSubmit } = this.props;
-    
+
         return (
-            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-                <div>
-                    Add new warranty
-                </div>
-                <div>
-                    <label>Name of the thing</label>
+            <div>
+                <Link to="/">Back</Link>
+
+                <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                     <div>
-                        <Field
-                            name="name"
-                            component="input"
-                            type="text"
-                            placeholder="Name of your purchase"
-                        />
+                        Add new warranty
                     </div>
-                </div>
-                <div>
-                    <label>Receipt of purchase</label>
                     <div>
-                        <Field
-                            name="receipt"
-                            component={FileInput}
-                            type="file"
-                            placeholder="Receipt of your purchase"
-                        />
+                        <label>Name of the thing</label>
+                        <div>
+                            <Field
+                                name="name"
+                                component="input"
+                                type="text"
+                                placeholder="Name of your purchase"
+                            />
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <label>Date of purchase</label>
                     <div>
-                        <Field
-                            name="date"
-                            component="input"
-                            type="date"
-                            placeholder="When did you buy it"
-                        />
+                        <label>Receipt of purchase</label>
+                        <div>
+                            <Field
+                                name="receipt"
+                                component={FileInput}
+                                type="file"
+                                placeholder="Receipt of your purchase"
+                            />
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <label>Warranty ends</label>
                     <div>
-                        <Field
-                            name="end_of_warranty"
-                            component="input"
-                            type="date"
-                            placeholder="When does the warranty end"
-                        />
+                        <label>Date of purchase</label>
+                        <div>
+                            <Field
+                                name="purchase_date"
+                                component="input"
+                                type="date"
+                                placeholder="When did you buy it"
+                            />
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <button type="submit">
-                    Submit
-                    </button>
-                </div>
-            </form>
+                    <div>
+                        <label>Warranty ends</label>
+                        <div>
+                            <Field
+                                name="end_of_warranty"
+                                component="input"
+                                type="date"
+                                placeholder="When does the warranty end"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <button type="submit">
+                        Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
         );
     }
 }
